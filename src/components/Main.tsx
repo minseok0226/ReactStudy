3b365d4ed5eb2ac621ceacfaa3783936a020b9ed
sikchang <changsik323@gmail.com>
2023-05-16 20:59:45 +0900

    Main-Page 상단에 simsimi 입력

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from '../images/logo.svg';
 import '../css/Main.css';
 
-function App() {
+
+function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>성창식</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main-header'>
+      <h1 id='SimSimi-header'>
+        SimSimi
+      </h1>
     </div>
   );
 }
 
-export default App;
+export default Main;
