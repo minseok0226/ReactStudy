f2c3fdfd90dcdbc7714647a43abf736124038ee1
sikchang <changsik323@gmail.com>
2023-05-17 18:29:38 +0900

    과제 진행중 .. 남은건 집에서 ...

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/Main.css';
 
 
 function Main() {
+
+  const [ApiText, setMessage] = useState('');
+
   return (
     <div id='main-header'>
-      <h2 id='SimSimi-header'>
-        왜 메인브랜치에 작업을 해서 막 푸시하는가 성창식
-      </h2>
+      <header>
+        <h2 id='SimSimi-header'>왜 메인브랜치에 작업을 해서 막 푸시하는가 성창식</h2>
+        <input
+          type='text'
+          name='ApiName'
+          placeholder='API 명을 입력하세요'
+          value={ApiText}
+          onChange={
+            (e) => {
+                setMessage(e.target.value);
+            }
+          }
+        />
+        <button onClick={
+          () => {
+            alert(ApiText);
+            setMessage('');
+          }
+        }>성창식 버튼
+        </button>
+      </header>
     </div>
   );
 }
