062f1c03dc5d06c0e573025ec623f1dc0a2b7d23
김민석(메가존) <mzmskim@lgupluspartners.co.kr>
2023-05-17 17:54:22 +0900

    성창식 -> 김민석으로 변경 ㅎㅎㅎ

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>성창식</h1>
+        <p>김민석ㅎㅎㅎ</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
