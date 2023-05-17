aa865fcf954e17aadda71678b47add99778ff613
김민석(메가존) <mzmskim@lgupluspartners.co.kr>
2023-05-17 17:59:02 +0900

    메세지 수정

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,9 +5,9 @@ import '../css/Main.css';
 function Main() {
   return (
     <div id='main-header'>
-      <h1 id='SimSimi-header'>
-        SimSimi
-      </h1>
+      <h2 id='SimSimi-header'>
+        왜 메인브랜치에 작업을 해서 막 푸시하는가 성창식
+      </h2>
     </div>
   );
 }
